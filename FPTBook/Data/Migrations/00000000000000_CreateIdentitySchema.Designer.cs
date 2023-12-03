2e62b1e03cb2a153ef5c061f8073105e3ab089bd
Phan Thắng <thangpdgcd201784@fpt.edu.vn>
2023-12-03 18:42:22 +0700

    Thang push

--- FPTBook/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ FPTBook/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -9,7 +9,7 @@ using System;
 
 namespace FPTBook.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(ApplicationUser))]
     [Migration("00000000000000_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
